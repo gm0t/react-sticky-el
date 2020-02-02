1984e3f7044491cddeed81f1e3f0a9359eff5060
tmilewski <tmilewski@gmail.com>
2020-02-02 15:13:32 -0500

    Update dependencies & Webpack configuration

--- webpack-examples.config.js
+++ webpack-examples.config.js
@@ -1,47 +1,41 @@
 var path = require('path');
-var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var baseConfig = require('./webpack.config');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-var entry = [];
-
-if (process.env.NODE_ENV !== 'production') {
-  entry = entry.concat([
-    'webpack-dev-server/client?http://localhost:3001',
-    'webpack/hot/only-dev-server',
-    'react-hot-loader/patch'
-  ]);
-}
-
-entry.push('./index');
-
-var plugins = baseConfig.plugins;
-plugins.push(new HtmlWebpackPlugin({template: 'index.html', inject: false}));
-
-if (process.env.NODE_ENV !== 'production') {
-  plugins = plugins.concat([
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]);
-}
+var NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = Object.assign(baseConfig, {
+  mode: NODE_ENV,
   context: __dirname + '/examples',
-  devtool: process.env.NODE_ENV === 'production' ? null : '#eval-source-map',
-  entry: entry,
-  externals: null,
+  devtool: NODE_ENV === 'production' ? false : 'inline-source-map',
+  devServer: {
+    port: 3001,
+    contentBase: './examples',
+    hot: true,
+    liveReload: false,
+  },
+  entry: ['./index'],
   output: {
     path: path.join(__dirname, 'dist', 'examples'),
     filename: 'bundle.js',
     publicPath: '/'
   },
-  plugins: plugins,
+  plugins: [
+    ...baseConfig.plugins,
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: false
+    })
+  ],
+  externals: [],
   resolve: {
     alias: {
       'react-sticky-el': path.join(__dirname, 'src')
     },
-    root: path.resolve('./app'),
-    extensions: ['', '.js']
+    modules: [
+      'node_modules',
+      path.resolve('./app')
+    ],
+    extensions: ['.js']
   }
 });
