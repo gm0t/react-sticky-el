a38df473babe89f752cc010b1d982de9f08cc672
Daniel da Silva <danielfdsilva@gmail.com>
2021-03-18 14:49:53 +0000

    Update documentation and add clipPath example story

--- stories/hiding.story.js
+++ stories/hiding.story.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Wrapper, createBlocks } from "./common";
 
 export default {
@@ -51,5 +51,48 @@ export const HideOnBoundaryHit3 = () => (
 
 HideOnBoundaryHit3.story = {
   name: 'false',
+};
+
+export const HideOnBoundaryHit4 = () => {
+  const [ useClipPath, setUseClipPath ] = useState(false);
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="column">
+          <div
+            className="scroll-area scroll-area--distant"
+            style={useClipPath ? { clipPath: 'inset(0 0 0 0)'} : undefined}
+          >
+            <p>
+              If the <code>scrollareaElement</code> is not at the top of the
+              viewport (shown here with 100px margin), when the sticky element
+              overflows it, it will stay visible (try scrolling).
+            </p>
+            <p>
+              You can use <code>clip-path: inset(0 0 0 0);</code> on the{' '}
+              <code>scrollareaElement</code> to solve this but may have
+              unintended consequences.
+            </p>
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={useClipPath}
+                  onChange={() => setUseClipPath(!useClipPath)}
+                />
+                  Use clip path
+                </label>
+            </p>
+            {createBlocks(false, 0, 0, false)}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+)
+};
+
+HideOnBoundaryHit4.story = {
+  name: 'overflow',
 };
 
